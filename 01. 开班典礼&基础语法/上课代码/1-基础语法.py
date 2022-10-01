9a52e0fb936878f989d0cf174fc88d4423b33c0c
ysamll <36656269@qq.com>
2022-10-01 22:08:48 +0800

    update 01

---
+++ 01. 开班典礼&基础语法/上课代码/1-基础语法.py
@@ -0,0 +1,70 @@
+# name = '牛牛'
+# int name = 12
+
+
+# age = 12
+# age = 13
+# print(age)
+
+
+# hobby
+
+
+# 同时为多个变量赋值
+# a, c = 1, 2
+# print(a)
+# print(c)
+
+
+# _857 = 90
+
+
+# True = 100
+# import keyword
+# print(keyword.kwlist)
+
+
+# h = 100
+
+
+# a = 100
+# print(a)
+
+
+# 单行注释 :从#号开始到这一行的末尾都不会执行
+# name = 19  print(name)
+# print(1)
+# print(1)
+# print(1)
+# print(1)
+# print(1)
+
+
+# 多行注释  只要是写在三对双引号或者单引号之间的东西都会被注释掉，不会被执行
+'''
+name = 10
+print(name)
+'''
+
+
+"""
+name = 10
+print(name)
+"""
+
+
+# 缩进
+# if条件判断
+"""
+if条件判断的语法：
+if 条件:
+    条件正确的时候执行的语句
+else:
+    条件错误的时候执行的语句
+"""
+age = 99
+if age > 10:
+    print('你大于10')
+    print('我要找大哥')
+else:
+    print('你也不行呀')
---
+++ 01. 开班典礼&基础语法/上课代码/3-输入.py
@@ -0,0 +1,5 @@
+content = input('请输入：')
+print('hello world')
+print(content)
+# str 只要是被一对双引号或者说单引号包裹的内容就是str字符串类型
+name = '1.0'
